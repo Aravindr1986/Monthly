7c652d9c538371d27410cc0a6bac97016720404e
Aravindr1986 <ramachandran.aravind@gmail.com>
2022-12-26 06:27:53 -0500

    initial version of expense add page

--- pages/api/expense.js
+++ pages/api/expense.js
@@ -2,12 +2,12 @@ import mongoClient from'../../api-lib/db/mongodb'
 export default async function expenseHandler(req, res) {
     var mongoclnt=await mongoClient
     const collectionName = 'expense';
-    var token="";
+    //var token="";
     if(req.method==='POST'){
         console.log(req.body)
         var db=mongoclnt.db();
-        const insertedExpense = await db    .collection(collectionName).insertOne(req.body);
-        console.log(userRecord);
+        const insertedExpense = await db.collection(collectionName).insertOne(req.body);
+        console.log(insertedExpense);
        
     }
 
