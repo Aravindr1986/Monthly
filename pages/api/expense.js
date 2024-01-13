d1325f647393c329558794eeb2d601af315801e4
Aravindr1986 <ramachandran.aravind@gmail.com>
2024-01-13 10:19:37 -0500

    Updated default Db names

--- pages/api/expense.js
+++ pages/api/expense.js
@@ -3,13 +3,12 @@ import mongoClient from'../../api-lib/db/mongodb'
 export default async function expenseHandler(req, res) {
     var mongoclnt=await mongoClient
     const collectionName = 'expense';
-    //var token="";
     var insertedExpense;
     if(req.method==='POST'){
         console.log(req.body)
         req.body.Amount=parseFloat(req.body.Amount)
         req.body.expenseDate=new Date(req.body.expenseDate);
-        var db=mongoclnt.db();
+        var db=mongoclnt.db('BUDGET-DB');
         insertedExpense= await db.collection(collectionName).insertOne(req.body);
         console.log(insertedExpense);
        
