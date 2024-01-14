433d8534a420a4a84b982156a7a44038d560065b
Aravindr1986 <ramachandran.aravind@gmail.com>
2024-01-14 13:49:03 -0500

    Updated query

--- pages/api/category.js
+++ pages/api/category.js
@@ -1,13 +1,16 @@
 import mongoClient from "../../api-lib/db/mongodb";
 export default async function categoryHandler(req, res) {
   var mongoclnt = await mongoClient;
-  const collectionName = "category";
-  //var token="";
+  const collectionName = "Category";
+  //var token="";'
+  console.log(req.method)
+  var expenseCategories=[]
   if (req.method === "GET") {
-    var db = mongoclnt.db();
+    var db = mongoclnt.db('BUDGET-DB');
     expenseCategories = await db
-      .collection(collectionName)
+      .collection(collectionName).find({})
       .toArray();
   }
+  console.log(expenseCategories)
   res.status(200).json(expenseCategories);
 }
