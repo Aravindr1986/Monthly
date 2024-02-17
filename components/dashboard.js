c323dcbe139789a2222e8c41ae109658446429e4
Aravindr1986 <ramachandran.aravind@gmail.com>
2024-02-17 18:37:56 -0500

    Added comoponets

---
+++ components/dashboard.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+export default    function dashboard() { 
+    return (
+      <div>
+      <Head>
+        <title>Monthly App</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <div>         
+              Welcome to the Monthly App
+          </div>
+          <br></br> 
+          <div>
+          <table>  
+            <tbody>
+              <tr><td><a href="http://localhost:3000/expense">Add an expense</a></td></tr>
+              <tr><td><a href="http://localhost:3000/aggregate">View monthly summary</a></td></tr>
+            </tbody>
+          </table>
+        </div>
+        </div>
+        
+      </main>
+    </div>
+    )
+  }
