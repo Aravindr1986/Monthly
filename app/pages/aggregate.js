4508527081947af7dfcb8663f547045e337e3939
Aravindr1986 <ramachandran.aravind@gmail.com>
2024-03-09 17:19:21 -0500

    Added initial state variables

--- app/pages/aggregate.js
+++ app/pages/aggregate.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default   function  Aggregate({data}) {
   const router = useRouter()
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
   
   return (
     <div>
