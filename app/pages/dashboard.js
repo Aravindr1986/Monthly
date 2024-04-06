f9ec8ecd45272a1cadc0468249ed0d0b7f8979a2
Aravindr1986 <ramachandran.aravind@gmail.com>
2024-04-06 06:13:45 -0400

    Converting to stand alone page

--- app/pages/dashboard.js
+++ app/pages/dashboard.js
@@ -2,7 +2,7 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import Expense from './expense'; // Import the Expense component
+import Expense from './components/expense'; // Import the Expense component
 import Aggregate from './components/aggregate'; // Import the Aggregate component
 import styles from '../styles/dashboard.module.css';
 
